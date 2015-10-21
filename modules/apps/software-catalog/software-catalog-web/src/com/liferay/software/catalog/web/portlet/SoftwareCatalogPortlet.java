fbb23a6121ca9e59c8c62020304c8e2efbe6d2ac
philip.jones <philip.jones@liferay.com>
2015-10-20 22:06:48 -0700

    LPS-59277 Move portlets into module along basic configuration

---
+++ modules/apps/software-catalog/software-catalog-web/src/com/liferay/software/catalog/web/portlet/SoftwareCatalogPortlet.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.software.catalog.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+/**
+ * @author Philip Jones
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.active=false",
+		"com.liferay.portlet.control-panel-entry-weight=11.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-software-catalog",
+		"com.liferay.portlet.friendly-url-mapping=software_catalog",
+		"com.liferay.portlet.friendly-url-routes=META-INF/friendly-url-routes/routes.xml",
+		"com.liferay.portlet.icon=/icons/software_catalog.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Software Repository",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SoftwareCatalogPortlet extends MVCPortlet {
+}
