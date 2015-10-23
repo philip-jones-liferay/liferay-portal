4a6dd258742da2504d5be6328368c237d05636fe
philip.jones <philip.jones@liferay.com>
2015-10-23 13:09:33 -0700

    LPS-59277 api files generated by serviceBuilder

---
+++ modules/apps/software-catalog/software-catalog-api/src/com/liferay/software/catalog/service/SCLicenseServiceWrapper.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.software.catalog.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link SCLicenseService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see SCLicenseService
+ * @generated
+ */
+@ProviderType
+public class SCLicenseServiceWrapper implements SCLicenseService,
+	ServiceWrapper<SCLicenseService> {
+	public SCLicenseServiceWrapper(SCLicenseService scLicenseService) {
+		_scLicenseService = scLicenseService;
+	}
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	@Override
+	public java.lang.String getBeanIdentifier() {
+		return _scLicenseService.getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	@Override
+	public void setBeanIdentifier(java.lang.String beanIdentifier) {
+		_scLicenseService.setBeanIdentifier(beanIdentifier);
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
+	 */
+	@Deprecated
+	public SCLicenseService getWrappedSCLicenseService() {
+		return _scLicenseService;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #setWrappedService}
+	 */
+	@Deprecated
+	public void setWrappedSCLicenseService(SCLicenseService scLicenseService) {
+		_scLicenseService = scLicenseService;
+	}
+
+	@Override
+	public SCLicenseService getWrappedService() {
+		return _scLicenseService;
+	}
+
+	@Override
+	public void setWrappedService(SCLicenseService scLicenseService) {
+		_scLicenseService = scLicenseService;
+	}
+
+	private SCLicenseService _scLicenseService;
+}
